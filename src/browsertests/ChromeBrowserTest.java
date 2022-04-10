b61beb1c6c2cc7454beaa5689bdd318e756d6c44
Chintan170218 <100502452+Chintan170218@users.noreply.github.com>
2022-04-10 22:27:49 +0100

    Update ChromeBrowserTest.java

--- src/browsertests/ChromeBrowserTest.java
+++ src/browsertests/ChromeBrowserTest.java
@@ -5,11 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class ChromeBrowserTest {
 
     public static void main(String[] args) {
